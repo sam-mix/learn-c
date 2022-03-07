36b5e9fda8a305e2a2a586f783348235ff09dac5
米鸿坤 <1045262335@qq.com>
2022-03-07 09:57:11 +0000

    x

--- hello.c
+++ hello.c
@@ -1,4 +1,4 @@
-/* hello.c  -- 输出hello world*/ 
+// /* hello.c  -- 输出hello world*/ 
 // #include <stdio.h>
 
 
@@ -7,23 +7,33 @@
 // 	return 0;
 // }
 
-/* showf_pt.c -- 以两种方式显示float类型的值*/
+// /* showf_pt.c -- 以两种方式显示float类型的值*/
 
+// #include <stdio.h>
+
+// int main(void) {
+// 	float about = 32000.0;
+// 	double abet = 2.14e9;
+// 	long double dip = 5.32e-5;
+
+// 	printf("%f can be written %e\n", about, about);
+
+// 	// 下一行要求编译器支持 C99 或其中相关特性
+// 	printf("And it's %a in hexadecimal, poers of 2 notation\n", about);
+
+// 	printf("%f can be writen %e\n", abet, abet);
+// 	printf("%Lf can be writen %Le\n", dip, dip);
+
+// 	return 0;
+// }
+
+/* floaterr,c -- 演示舍入错误 */
 #include <stdio.h>
 
 int main(void) {
-	float about = 32000.0;
-	double abet = 2.14e9;
-	long double dip = 5.32e-5;
-
-	printf("%f can be written %e\n", about, about);
-
-	// 下一行要求编译器支持 C99 或其中相关特性
-	printf("And it's %a in hexadecimal, poers of 2 notation\n", about);
-
-	printf("%f can be writen %e\n", abet, abet);
-	printf("%Lf can be writen %Le\n", dip, dip);
-
+	float a, b;
+	a = 2.0e20 + 1.0;
+	a = b - 2.0e20;
+	printf("%f\n", a);
 	return 0;
 }
-
