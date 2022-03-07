30201bc4c6034c1ec9dd0ec8e47caff2fa7bac89
米鸿坤 <1045262335@qq.com>
2022-03-07 09:53:34 +0000

    x

--- hello.c
+++ hello.c
@@ -1,7 +1,29 @@
+/* hello.c  -- 输出hello world*/ 
+// #include <stdio.h>
+
+
+// int main() {
+// 	printf("Hello World!\n");
+// 	return 0;
+// }
+
+/* showf_pt.c -- 以两种方式显示float类型的值*/
+
 #include <stdio.h>
 
+int main(void) {
+	float about = 32000.0;
+	double abet = 2.14e9;
+	long double dip = 5.32e-5;
 
-int main() {
-	printf("Hello World!\n");
+	printf("%f can be written %e\n", about, about);
+
+	// 下一行要求编译器支持 C99 或其中相关特性
+	printf("And it's %a in hexadecimal, poers of 2 notation\n", about);
+
+	printf("%f can be writen %e\n", abet, abet);
+	printf("%Lf can be writen %Le\n", dip, dip);
+
 	return 0;
 }
+
